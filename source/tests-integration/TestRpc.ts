633c380ffe74245a15fe063e8473229fb07fd02e
Mike Seese <seesemichaelj@gmail.com>
2018-02-12 22:54:22 -0500

    get testrpc interface working with sdb

--- source/tests-integration/TestRpc.ts
+++ source/tests-integration/TestRpc.ts
@@ -8,14 +8,17 @@ const TestRPC = require("../../../ganache-core");
 export class TestRpc {
     private readonly DEFAULT_TEST_ACCOUNT_BALANCE = 10**20;
     private readonly BLOCK_GAS_LIMIT = 6500000;
+    private readonly BLOCK_GAS_LIMIT_SDB = 10000000;
     private readonly configuration: Configuration;
     private testRpcServer: any;
 
     constructor(configuration: Configuration) {
         this.configuration = configuration;
 
+        const blockGasLimit = this.configuration.enableSdb ? this.BLOCK_GAS_LIMIT_SDB : this.BLOCK_GAS_LIMIT;
+
         const accounts = [{ balance: `0x${this.DEFAULT_TEST_ACCOUNT_BALANCE.toString(16)}`, secretKey: configuration.privateKey }];
-        const options = { gasLimit: `0x${this.BLOCK_GAS_LIMIT.toString(16)}`, accounts: accounts, sdb: this.configuration.enableSdb };
+        const options = { gasLimit: `0x${blockGasLimit.toString(16)}`, accounts: accounts, sdb: this.configuration.enableSdb };
         this.testRpcServer = TestRPC.server(options);
         this.testRpcServer.listen(configuration.httpProviderPort);
     }
@@ -33,13 +36,12 @@ export class TestRpc {
 
     public linkDebugSymbols = async (compilerOutput: CompilerOutput, addressMapping: any): Promise<void> => {
         const sdbHook = this.testRpcServer.provider.manager.state.sdbHook;
-        if (sdbHook && addressMapping.length > 0) {
-            const mapping = addressMapping[addressMapping.length - 1];
+        if (sdbHook) {
             sdbHook.linkCompilerOutput(this.configuration.contractSourceRoot, compilerOutput);
-            const keys = Object.keys(mapping);
+            const keys = Object.keys(addressMapping);
             for (let i = 0; i < keys.length; i++) {
                 const contractName = keys[i];
-                sdbHook.linkContractAddress(contractName, mapping[keys[i]]);
+                sdbHook.linkContractAddress(contractName, addressMapping[keys[i]]);
             }
         }
     }
